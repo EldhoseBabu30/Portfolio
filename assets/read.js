7cf340b451629c2636bda5d9cee6a254567b0081
Eldhose Babu <90978097+EldhoseBabu30@users.noreply.github.com>
2023-04-06 12:34:46 +0530

    Add files via upload

---
+++ assets/read.js
@@ -0,0 +1,113 @@
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "inline";
+    }
+  }
+  //css
+  function myFunctionCss() {
+    var dots = document.getElementById("dots-css");
+    var moreText = document.getElementById("more-css");
+    var btnText = document.getElementById("myBtnCss");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "inline";
+    }
+  }
+  //js
+  function myFunctionJs() {
+    var dots = document.getElementById("dots-js");
+    var moreText = document.getElementById("more-js");
+    var btnText = document.getElementById("myBtnJs");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "inline";
+    }
+  }
+  //node
+  function myFunctionNode() {
+    var dots = document.getElementById("dots-node");
+    var moreText = document.getElementById("more-node");
+    var btnText = document.getElementById("myBtnNode");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "inline";
+    }
+  }
+  //angular
+  function myFunctionAngular() {
+    var dots = document.getElementById("dots-angular");
+    var moreText = document.getElementById("more-angular");
+    var btnText = document.getElementById("myBtnAngular");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "inline";
+    }
+  }
+  //react js
+  function myFunctionReact() {
+    var dots = document.getElementById("dots-react");
+    var moreText = document.getElementById("more-react");
+    var btnText = document.getElementById("myBtnReact");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "inline";
+    }
+  }
+
+  //mongo db
+  function myFunctionMongo() {
+    var dots = document.getElementById("dots-mongo");
+    var moreText = document.getElementById("more-mongo");
+    var btnText = document.getElementById("myBtnMongo");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "inline";
+    }
+  }
+
